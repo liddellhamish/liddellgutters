5ba58b950971587b83ad56f90a4154711cc72e85
liddellhamish <liddellhamish@gmail.com>
2025-07-22 16:29:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,7 +19,4 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navLinks = document.getElementById("navLinks");
 
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-});
+ 
